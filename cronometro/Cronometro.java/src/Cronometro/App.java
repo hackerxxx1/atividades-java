894465487f3dd824cbe4957a92e25787cc0a09cc
hackerxxx <43994798+hackerxxx1@users.noreply.github.com>
2022-09-21 17:56:34 -0300

    subindo projeto pronto

---
+++ cronometro/Cronometro.java/src/Cronometro/App.java
@@ -0,0 +1,12 @@
+package Cronometro;
+
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+        });
+        new Cronometro();
+    }
+}
--- cronometro/Cronometro.java/src/Cronometro/Cronometro.java
+++ cronometro/Cronometro.java/src/Cronometro/Cronometro.java
@@ -7,6 +7,7 @@ package Cronometro;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
 
 public class Cronometro implements ActionListener {
   
@@ -23,24 +24,37 @@ public class Cronometro implements ActionListener {
         public Cronometro() {
             //criando tela
             JFrame tela = new JFrame("tela top demais");
+            //criando o rotulo da janela
+            rotulo = new JLabel("aperta ai pra começar");
             //adicionar evento 
             btnStart.addActionListener(this);
             btnStop.addActionListener(this);
             //layout flow
             tela.setLayout(new FlowLayout());
-            //adicionando no container
+            //adicionando no container todos os componentes
             tela.add(btnStart);
             tela.add(btnStop);
+            tela.add(rotulo);
             //colocando tamanho do swing
             tela.setSize(400,400);
             //seta condição de saida da janela
             tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //liberando para visualização'
+            tela.setVisible(true);
         }
 
     
         public void actionPerformed(ActionEvent e) {
             
-            
+            //obter hora sistema
+            Calendar cal = Calendar.getInstance();
+            // armazenar hora
+            if (e.getActionCommand().equals("começar")) {
+                start = cal.getTimeInMillis();
+                rotulo.setText("cronometro esta rodando");
+            }else{
+                rotulo.setText("o tempo contado foi de:"+(double)(cal.getTimeInMillis()-start)/1000);
+            }
         }
      
    
