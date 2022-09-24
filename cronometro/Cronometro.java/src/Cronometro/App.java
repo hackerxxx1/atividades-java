1da27e4be0ff7c7f5d5994005a914a9994ca16a8
hackerxxx <43994798+hackerxxx1@users.noreply.github.com>
2022-09-23 23:30:37 -0300

    tela cronometro

--- cronometro/Cronometro.java/src/Cronometro/App.java
+++ cronometro/Cronometro.java/src/Cronometro/App.java
@@ -1,12 +1,23 @@
 package Cronometro;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) throws Exception {
         SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            new Cronometro();
             
+        }
         });
-        new Cronometro();
+        String url = "jdbc:mysql://localhost/jeduk?useTimeZone=true&serverTimezone=UTC";
+        String user = "root";
+        String password= "root123";
+
+        Connection con = DriverManager.getConnection(url, user, password);
+        con.close();
     }
 }
